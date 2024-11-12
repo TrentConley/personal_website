3abac54f814c9a88d050793910a3ad9a4ebe5a59
Trent Conley <trentconley@machindustries.com>
2024-11-12 14:09:54 -0500

    redirect / lunch to ensworth menu

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/lunch',
+                destination: 'https://www.sagedining.com/sites/ensworthschool/menu',
+                permanent: true, // Use `false` if you want a temporary redirect (307)
+            },
+        ];
+    },
+};
 
 export default nextConfig;
